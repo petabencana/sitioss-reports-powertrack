fc5acac3ad389bff5d71e2bfa5753a2c2c1f1a3d
Tomas <tomasholderness@gmail.com>
2017-08-22 14:44:02 -0400

    update powertrack config to Indonesia by default

--- powertrack-config.js
+++ powertrack-config.js
@@ -49,9 +49,10 @@ config.gnip.streamUrl = process.env.GNIP_STREAM_URL; // Gnip stream URL, take fr
 config.gnip.rulesUrl = process.env.GNIP_RULES_URL; // Gnip rules URL, take from the Gnip admin interface.
 // Gnip rules, enter as an object where the key is the rule name and the value is the rule as a string
 config.gnip.rules = {
-    "boundingbox":"(contains:flood OR contains:rains OR contains:monsoon prep) (bounding_box:[80.0900 12.8400 80.3800 13.0517] OR bounding_box:[80.0900 13.0517 80.3800 13.2555])",
-    "addressed":"(contains:flood OR contains:rains OR contains:monsoon prep) @riskmapindia",
-    "location":"(contains:flood OR contains:rains OR contains:monsoon prep) (bio_location:chennai OR place:chennai OR bounding_box:[80.0900 12.8400 80.3800 13.0517] OR bounding_box:[80.0900 13.0517 80.3800 13.2555])"
+  "addressed": "(contains:flood OR contains:banjir OR contains:jakartabanjir) @petabencana OR (contains:flood OR contains:banjir OR contains:jakartabanjir) @petajkt",
+      "jbd":"( contains:flood OR contains:banjir OR contains:jakartabanjir ) ( bounding_box:[106.471 -6.19140 106.79381 -5.880] OR bounding_box:[106.79381 -6.19140 107.10880 -5.880] OR bounding_box:[107.10880 -6.19140 107.175 -5.880] OR bounding_box:[106.471 -6.50213 106.79381 -6.19140] OR bounding_box:[106.79381 -6.50213 107.10880 -6.19140] OR bounding_box:[107.10880 -6.50213 107.175 -6.19140] OR bounding_box:[106.471 -6.733 106.79381 -6.50213] OR bounding_box:[106.79381 -6.733 107.10880 -6.50213] OR bounding_box:[107.10880 -6.733 107.175 -6.50213] OR bio_location:jakarta OR place:jakarta)",
+      "bdg":"( contains:flood OR contains:banjir OR contains:jakartabanjir ) ( bounding_box:[107.369 -6.97964 107.68291 -6.668] OR bounding_box:[107.68921 -6.97964 107.931 -6.668] OR bounding_box:[107.369 -7.165 107.68921 -6.97964] OR bounding_box:[107.68291 -7.165 107.931 -6.97962] OR bio_location:bandung OR place:bandung)",
+      "sby":"( contains:flood OR contains:banjir OR contains:jakartabanjir ) ( bounding_box:[112.3975 -7.32570 112.71169 -7.0143] OR bounding_box:[112.71169 -7.32570 113.0318 -7.0143] OR bounding_box:[112.3975 -7.5499 112.71169 -7.32570] OR bounding_box:[112.71169 -7.5499 113.0318 -7.32570] OR bio_location:surabaya OR place:surabaya)"
 };
 
 config.gnip.maxReconnectTimeout = 1000 * 60 * 5; // In milliseconds; 5 minutes for max reconnection timeout - will mean ~10 minutes from first disconnection
@@ -83,9 +84,9 @@ config.twitter.dialogue.ahoy = {};          // Greet users
 config.twitter.dialogue.requests = {};      // Respond to user requests
 config.twitter.dialogue.requests.card = {};  // Flood report card responses
 // Dialogue translations
-config.twitter.dialogue.ahoy.en = 'Hello, I am RiskMapBot, reply with #flood to send me your flood report.';
-config.twitter.dialogue.ahoy.id =  'Halo, saya RiskMapBot. Untuk melaporkan banjir di sekitarmu, silakan balas dengan #banjir.';
-config.twitter.dialogue.ahoy.in = 'Hello, I am RiskMapBot, reply with #flood to send me your flood report.';
+config.twitter.dialogue.ahoy.en = 'Hello, I am Bencana Bot, reply with #flood to send me your flood report.';
+config.twitter.dialogue.ahoy.id =  'Halo, saya Bencana Bot. Untuk melaporkan banjir di sekitarmu, silakan balas dengan #banjir.';
+config.twitter.dialogue.ahoy.in = 'Hello, I am Bencana Bot, reply with #flood to send me your flood report.';
 config.twitter.dialogue.requests.card.en = 'Hi! Report flood using this link. Thanks!';
 config.twitter.dialogue.requests.card.id = 'Hai! Gunakan link ini untuk menginput lokasi banjir, keterangan, & foto.';
 config.twitter.dialogue.requests.card.in = 'Hi! Report flood using this link. Thanks!';
